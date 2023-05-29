668360fd61b0d209aaa11705cd9176144ea4a25f
Tim Andracek <tandracek@Tims-MacBook-Air.local>
2023-05-29 13:49:47 -0500

    Formatting.

--- todo-client/src/components/TodoSection.test.js
+++ todo-client/src/components/TodoSection.test.js
@@ -3,36 +3,36 @@ import Client from '../client';
 import TodoSection from './TodoSection';
 
 describe("TodoSection", () => {
-    const client = new Client("mockme");
-    test("Create and update", async () => {
-        jest.spyOn(client, "createTodo")
-            .mockImplementation(todoTemplate => (Promise.resolve({...todoTemplate, id: 1, createdAt: "", modifiedAt: ""})));
+  const client = new Client("mockme");
+  test("Create and update", async () => {
+    jest.spyOn(client, "createTodo")
+      .mockImplementation(todoTemplate => (Promise.resolve({...todoTemplate, id: 1, createdAt: "", modifiedAt: ""})));
 
-        jest.spyOn(client, "updateTodo")
-            .mockImplementation((id, todo) => {
-                expect(id).toBe(1);
-                return Promise.resolve(todo);
-            });
+    jest.spyOn(client, "updateTodo")
+      .mockImplementation((id, todo) => {
+          expect(id).toBe(1);
+          return Promise.resolve(todo);
+      });
 
-        const todoSection = render(<TodoSection client={client} />);
-        const createInput = todoSection.getByRole("textbox", {id: "create-input"});
-        fireEvent.change(createInput, {target: {value: "Test TODO"}});
-        const createButton = todoSection.getByRole("button", {id: "create-button"});
-        fireEvent.click(createButton);
+    const todoSection = render(<TodoSection client={client} />);
+    const createInput = todoSection.getByRole("textbox", {id: "create-input"});
+    fireEvent.change(createInput, {target: {value: "Test TODO"}});
+    const createButton = todoSection.getByRole("button", {id: "create-button"});
+    fireEvent.click(createButton);
 
-        await waitFor(() => {
-            const counter = todoSection.getByTitle("counter");
+    await waitFor(() => {
+      const counter = todoSection.getByTitle("counter");
 
-            todoSection.getByText("Test TODO");
-            expect(counter.innerHTML).toBe("TODOS: 1");
-        });
-
-        let completedCheckbox = todoSection.getByRole("checkbox", {id: "completed-1"});
-        fireEvent.click(completedCheckbox);
-
-        await waitFor(async () => {
-            completedCheckbox = await todoSection.getByRole("checkbox", {id: "completed-1"});
-            expect(completedCheckbox.checked).toBe(true);
-        });
+      todoSection.getByText("Test TODO");
+      expect(counter.innerHTML).toBe("TODOS: 1");
     });
+
+    let completedCheckbox = todoSection.getByRole("checkbox", {id: "completed-1"});
+    fireEvent.click(completedCheckbox);
+
+    await waitFor(async () => {
+      completedCheckbox = await todoSection.getByRole("checkbox", {id: "completed-1"});
+      expect(completedCheckbox.checked).toBe(true);
+    });
+  });
 });
